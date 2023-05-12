fdecf9b91a35083a583f7d8e4fbc2e5f77429763
Tlustrep <129354931+godololasisi@users.noreply.github.com>
2023-05-12 14:41:33 -0500

    Add files via upload

---
+++ Create_SQL_Oracle.sql
@@ -0,0 +1,33 @@
+  CREATE OR REPLACE NONEDITIONABLE PROCEDURE "SYSTEM"."GETCLIENTBYPARAMETER" (
+    vRuc    IN Client.Ruc%TYPE,
+    vCodigo         OUT NUMBER,
+    vMensaje            OUT VARCHAR2,
+    vRazonSocial OUT VARCHAR2
+)
+AS
+vCount NUMBER;
+BEGIN
+    vCodigo  := 1;
+    vMensaje := 'OK';
+    vRazonSocial := '';
+
+    SELECT 
+    COUNT(1) INTO vCount
+    FROM CLIENT CL;
+
+    IF vCount >= 1 THEN -- EXISTE EL USUARIO
+        Select 
+        cl.RazonSocial
+        INTO vRazonSocial
+        From Client cl
+        INNER JOIN Campus ca on cl.codigocampus = ca.codigo
+        Where cl.Ruc = vRuc;
+        RETURN;
+    ELSE    -- NO EXISTE USUARIO
+        vCodigo := 0;
+        vMensaje := 'No existe cliente.';
+        RETURN;
+    END IF; 
+END;
+
+/
